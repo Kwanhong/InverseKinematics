1deb16809eb56da0861d884398f33b6f7d7bfddf
BBloute <rhksghd1234@gmail.com>
2019-06-29 17:37:44 +0900

    관절 연결 구현

--- Project/Scripts/Game.cs
+++ Project/Scripts/Game.cs
@@ -4,11 +4,16 @@ using SFML.Window;
 using SFML.Audio;
 using SFML.System;
 using static InversKinematics.Data;
+using static InversKinematics.Constants;
+using static InversKinematics.Utility;
 
 namespace InversKinematics
 {
     public class Game
     {
+        private Segment segment1;
+        private Segment segment2;
+
         public Game()
         {
             Initialize();
@@ -20,6 +25,9 @@ namespace InversKinematics
             window.SetFramerateLimit(20);
             window.Closed += OnClose;
             window.KeyPressed += OnKeyPressed;
+
+            segment1 = new Segment(winSizeX / 2, winSizeY / 2, 50, DegreeToRadian(-45));
+            segment2 = new Segment(segment1, 50, DegreeToRadian(-45));
         }
 
         private void Run()
@@ -39,11 +47,17 @@ namespace InversKinematics
 
         private void Update()
         {
-
+            segment1.Wiggle();
+            segment1.Update();
+            segment2.Wiggle();
+            segment2.Update();
         }
 
         private void Render()
         {
+            segment1.Display();
+            segment2.Display();
+            
             window.Display();
             window.Clear(new Color(46, 46, 46));
         }
